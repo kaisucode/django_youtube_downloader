c648bd333023a7cbaa9f9b66ff5116061649c498
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-01 01:52:50 +0800

    Set homepage location in server

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('downloader/', include('downloader.urls')),
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='home'),
     path('admin/', admin.site.urls),
 ]
