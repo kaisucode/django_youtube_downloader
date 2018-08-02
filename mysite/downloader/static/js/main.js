b1a5bdfec0ad15d7d7006db017bc6bbf5f504449
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-02 15:25:00 +0800

    Going to try threading

---
+++ mysite/downloader/static/js/main.js
@@ -0,0 +1,21 @@
+
+window.onload = function (){
+
+	// document.addEventListener("keydown", function onEvent(event) {
+	//   var pauseEnter = {{ shouldPauseEnter }}
+
+	//   // Key lag
+	//   if (event.which === 13){
+	//     if( keyDownPressed === true )	return false;
+	//     keyDownPressed = true;
+	//     setTimeout(function() { keyDownPressed = false }, 200);
+	//   }
+
+	//   else if (event.which === 13)											// Enter
+
+	//   lastKey = event.which;
+	// });
+
+};
+
+
--- mysite/downloader/views.py
+++ mysite/downloader/views.py
@@ -1,8 +1,11 @@
 
+from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
+import youtube_dl
+import time
 
 def index(request): 
     return render(request, "homepage.html")
@@ -11,9 +14,22 @@ def index(request):
 def submit_link(request): 
     try:
         ytLink = request.GET["url_input"]
-        #  fout = open("hai.txt", "w")
-        #  fout.write(ytLink)
-        #  fout.close()
+
+        currentTime = time.strftime("%Y%m%d-%H%M%S")
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl': currentTime+'/%(title)s.%(ext)s',
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(ytLink, download=True)
+
+
         return render(request, "homepage.html", {"displayErrorVar": False})
     except Exception as e:
         return render(request, "homepage.html", {'displayErrorVar': True})
