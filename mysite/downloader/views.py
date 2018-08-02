ee665b26e6711184a2dbfc201c9b8a8216e90e10
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-02 14:17:44 +0800

    Finished routing apps, working on yt download next

--- mysite/downloader/views.py
+++ mysite/downloader/views.py
@@ -1,17 +1,20 @@
 
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 def index(request): 
-    return HttpResponse("Hello world, you're at my illegal website. ")
+    return render(request, "homepage.html")
+    #  return HttpResponse("Hello world, you're at my illegal website. ")
 
 def submit_link(request): 
     print("hello world")
     if request.method == 'GET': 
         fout = open("hai.txt", "w")
-        ytLink = request.GET["text"]
+        #  ytLink = request.GET["text"]
+        ytLink = request.GET["url_input"]
         fout.write(ytLink)
         fout.close()
-        return render(request, TemplateView.as_view(template_name='homepage.html/'))
+        return render(request, "homepage.html")
 
