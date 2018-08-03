ed88c9a882719442243194481d93ede65aec0f7a
Shadowsinger <kevin.hsukaihao@gmail.com>
2018-08-04 02:12:13 +0800

    Download works with test.mp3

--- mysite/downloader/views.py
+++ mysite/downloader/views.py
@@ -5,7 +5,11 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.conf import settings
 
+from wsgiref.util import FileWrapper
 
+
+
+import os
 import youtube_dl
 
 def index(request): 
@@ -26,12 +30,24 @@ def submit_link(request):
             'outtmpl': 'mp3/%(title)s.%(ext)s',
         }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info(ytLink, download=True)
-        #  songFilename = settings.BASE_DIR + "/mp3/" + result.get("title", None)+".mp3"
-        songFilename = settings.BASE_DIR + "/mp3/test.mp3"
+        #  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        #      result = ydl.extract_info(ytLink, download=True)
 
-        return render(request, "homepage.html", {"displayErrorVar": False, "shouldPauseEnter": "false", "song_url": songFilename})
+        #  filePath = settings.BASE_DIR + "/mp3/" + result.get("title", None)+".mp3"
+        filePath = settings.BASE_DIR + "/mp3/test.mp3"
+
+
+        contents = open(filePath, "rb")
+        wrapper = FileWrapper( contents )
+
+        response = HttpResponse(wrapper, content_type = "audio/mpeg")
+        response['Content-Length'] = os.path.getsize( filePath ) # not FileField instance
+        response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename( filePath )
+        return response
+
+
+
+        #  return render(request, "homepage.html", {"displayErrorVar": False, "shouldPauseEnter": "false", "song_url": songFilename})
     except Exception as e:
         print(e)
         print("exception")
